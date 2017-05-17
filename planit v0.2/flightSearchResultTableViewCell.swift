88956aa3572bbeb587dbe988fc07a0b34f7654d1
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2017-05-17 12:22:31 -0400

    searchResultsTableView

---
+++ planit v0.2/flightSearchResultTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  flightSearchResultTableViewCell.swift
+//  planit v0.2
+//
+//  Created by MICHAEL WURM on 5/17/17.
+//  Copyright © 2017 MICHAEL WURM. All rights reserved.
+//
+
+import UIKit
+
+class flightSearchResultTableViewCell: UITableViewCell {
+    
+    // MARK: Outlets
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+
+}
