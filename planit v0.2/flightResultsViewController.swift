6f6a0923b2e2a0fa42abe34d095b5b7d6ee54123
mwurm93 <michaelwurm@MICHAELs-MacBook-Pro.local>
2017-05-28 21:54:17 -0400

    smcalloutview Filter and Sort finished

--- planit v0.2/flightResultsViewController.swift
+++ planit v0.2/flightResultsViewController.swift
@@ -17,6 +17,11 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
     var selectedIndex = IndexPath(row: 0, section: 0)
     var sectionTitles = ["Selected flight", "Alternatives"]
     var sortFilterFlightsCalloutView = SMCalloutView()
+    let sortFilterFlightsCalloutTableView = UITableView(frame: CGRect.zero, style: .plain)
+
+    var calloutTableViewMode = "sort"
+    let sortFirstLevelOptions = ["Price","Duration","Landing Time", "Departure Time"]
+    let filterFirstLevelOptions = ["Stops","Airlines"]
     
     //MARK: Outlets
     @IBOutlet weak var flightResultsTableView: UITableView!
@@ -29,6 +34,14 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
         self.sortFilterFlightsCalloutView.delegate = self
         self.sortFilterFlightsCalloutView.isHidden = true
         
+        sortFilterFlightsCalloutTableView.delegate = self
+        sortFilterFlightsCalloutTableView.dataSource = self
+        sortFilterFlightsCalloutTableView.frame = CGRect(x: 0, y: 121, width: 100, height: 100)
+        sortFilterFlightsCalloutTableView.isEditing = false
+        sortFilterFlightsCalloutTableView.allowsMultipleSelection = false
+        sortFilterFlightsCalloutTableView.backgroundColor = UIColor.clear
+        sortFilterFlightsCalloutTableView.layer.backgroundColor = UIColor.clear.cgColor
+        
         //Set up table
         flightResultsTableView.tableFooterView = UIView()
         flightResultsTableView.isEditing = true
@@ -39,56 +52,71 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
     
     // MARK: UITableviewdelegate
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        if tableView == flightResultsTableView {
+            return 2
+        }
+        // else if tableView == sortFilterFlightsCalloutTableView
+        return 1
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
+        if tableView == flightResultsTableView {
+            if section == 0 {
+                return 1
+            }
+            // if section == 1
+            let numberOfRows = flightResultsDictionary.count
+            return numberOfRows - 1
         }
-        // if section == 1
-        let numberOfRows = flightResultsDictionary.count
-        return numberOfRows - 1
+        // else if tableView == sortFilterFlightsCalloutTableView
+        else if calloutTableViewMode == "sort" {
+            return sortFirstLevelOptions.count
+        }
+        return filterFirstLevelOptions.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if (selectedIndex == indexPath) {
-            return 104
-        } else {
-            return 52
+        if tableView == flightResultsTableView {
+            if (selectedIndex == indexPath) {
+                return 104
+            } else {
+                return 52
+            }
         }
+        // else if tableView == sortFilterFlightsCalloutTableView
+        return 22
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "flightSearchResultTableViewPrototypeCell", for: indexPath) as! flightSearchResultTableViewCell
-        cell.selectionStyle = .none
-        
-        //Change hamburger icon
-        for view in cell.subviews as [UIView] {
-            if type(of: view).description().range(of: "Reorder") != nil {
-                for subview in view.subviews as! [UIImageView] {
-                    if subview.isKind(of: UIImageView.self) {
-                        subview.image = UIImage(named: "hamburger")
+        if tableView == flightResultsTableView {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "flightSearchResultTableViewPrototypeCell", for: indexPath) as! flightSearchResultTableViewCell
+            cell.selectionStyle = .none
+            
+            //Change hamburger icon
+            for view in cell.subviews as [UIView] {
+                if type(of: view).description().range(of: "Reorder") != nil {
+                    for subview in view.subviews as! [UIImageView] {
+                        if subview.isKind(of: UIImageView.self) {
+                            subview.image = UIImage(named: "hamburger")
+                        }
                     }
                 }
             }
-        }
-        
-        if indexPath == IndexPath(row: 0, section: 0) {
-            cell.backgroundColor = UIColor.blue
-        } else {
-            cell.backgroundColor = UIColor.clear
-        }
-        
-        var flightsForRow = flightResultsDictionary[0]
-        
-        
-        var addedRow = indexPath.row + 1
-        if indexPath.section == 1 {
-            flightsForRow = flightResultsDictionary[addedRow]
-            addedRow += 1
-        }
-        
+            
+            if indexPath == IndexPath(row: 0, section: 0) {
+                cell.backgroundColor = UIColor.blue
+            } else {
+                cell.backgroundColor = UIColor.clear
+            }
+            
+            var flightsForRow = flightResultsDictionary[0]
+            
+            
+            var addedRow = indexPath.row + 1
+            if indexPath.section == 1 {
+                flightsForRow = flightResultsDictionary[addedRow]
+                addedRow += 1
+            }
+            
             cell.departureOrigin.text = flightsForRow["departureOrigin"]
             cell.departureDestination.text = flightsForRow["departureDestination"]
             cell.departureDepartureTime.text = flightsForRow["departureDepartureTime"]
@@ -98,18 +126,46 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
             cell.returnArrivalTime.text = flightsForRow["returnArrivalTime"]
             cell.returnDestination.text = flightsForRow["returnDestination"]
             cell.totalPrice.text = flightsForRow["totalPrice"]
+            
+            return cell
+        }
+        // else if tableView == sortFilterFlightsCalloutTableView
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "cellID")
         
-        return cell
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cellID")
+        }
+        
+        if calloutTableViewMode == "filter" {
+            cell?.textLabel?.text = filterFirstLevelOptions[indexPath.row]
+        } else if calloutTableViewMode == "sort" {
+            cell?.textLabel?.text = sortFirstLevelOptions[indexPath.row]
+        }
+        
+        cell?.textLabel?.textColor = UIColor.black
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 15)
+        cell?.textLabel?.numberOfLines = 0
+        cell?.backgroundColor = UIColor.clear
+        
+        return cell!
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if selectedIndex == indexPath {
-            selectedIndex = IndexPath(row: 0, section: 0)
-        } else {
-            selectedIndex = indexPath
-        }
+        if tableView == flightResultsTableView {
+            if selectedIndex == indexPath {
+                selectedIndex = IndexPath(row: 0, section: 0)
+            } else {
+                selectedIndex = indexPath
+            }
             self.flightResultsTableView.beginUpdates()
             self.flightResultsTableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
             self.flightResultsTableView.endUpdates()
+        } else if tableView == sortFilterFlightsCalloutTableView {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.25, execute: {
+                self.sortFilterFlightsCalloutView.dismissCallout(animated: true)
+                self.sortFilterFlightsCalloutView.isHidden = true
+                //HANDLE SORT / FILTER SELECTION
+            })
+        }
     }
     func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
     }
@@ -124,7 +180,11 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
     }
     
     func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return true
+        if tableView == flightResultsTableView {
+            return true
+        }
+        // else if tableView == sortFilterFlightsCalloutTableView
+        return false
     }
     
     func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
@@ -154,27 +214,37 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
     
     // MARK: Table Section Headers
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sectionTitles[section]
+        if tableView == flightResultsTableView {
+            return sectionTitles[section]
+        }
+        // else if tableView == sortFilterFlightsCalloutTableView
+        return nil
     }
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView?
     {
-        let header = UIView(frame: CGRect(x: 0, y: 0, width: flightResultsTableView.bounds.size.width, height: 30))
-        header.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
-        header.layer.cornerRadius = 5
-        
-        let title = UILabel()
-        title.frame = CGRect(x: 10, y: header.frame.minY, width: header.frame.width, height: header.frame.height)
-        title.textAlignment = .left
-        title.font = UIFont.boldSystemFont(ofSize: 20)
-        title.textColor = UIColor.white
-        title.text = sectionTitles[section]
-        header.addSubview(title)
-        
-        return header
+        if tableView == flightResultsTableView {
+            let header = UIView(frame: CGRect(x: 0, y: 0, width: flightResultsTableView.bounds.size.width, height: 30))
+            header.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
+            header.layer.cornerRadius = 5
+            
+            let title = UILabel()
+            title.frame = CGRect(x: 10, y: header.frame.minY, width: header.frame.width, height: header.frame.height)
+            title.textAlignment = .left
+            title.font = UIFont.boldSystemFont(ofSize: 20)
+            title.textColor = UIColor.white
+            title.text = sectionTitles[section]
+            header.addSubview(title)
+            
+            return header
+        }
+        return nil
     }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 30
+        if tableView == flightResultsTableView {
+            return 30
+        }
+        return CGFloat.leastNormalMagnitude
     }
 
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -183,24 +253,39 @@ class flightResultsViewController: UIViewController, UITableViewDelegate, UITabl
     
     //MARK: Actions
     @IBAction func filterFlightsButtonTouchedUpInside(_ sender: Any) {
-        
-        if self.sortFilterFlightsCalloutView.isHidden == true {
-        self.sortFilterFlightsCalloutView.title = "Filter results"
-        self.sortFilterFlightsCalloutView.isHidden = false
-        self.sortFilterFlightsCalloutView.animation(withType: .stretch, presenting: true)
-        self.sortFilterFlightsCalloutView.permittedArrowDirection = .up
-        var calloutRect: CGRect = CGRect.zero
-        calloutRect.origin = CGPoint(x: filterButton.frame.midX, y: CGFloat(59))
-        
-        calloutRect.size = CGSize.zero
-        self.sortFilterFlightsCalloutView.presentCallout(from: calloutRect, in: self.view, constrainedTo: self.view, animated: true)
+        if self.sortFilterFlightsCalloutView.isHidden == true || (self.sortFilterFlightsCalloutView.isHidden == false && calloutTableViewMode == "sort") {
+            calloutTableViewMode = "filter"
+            sortFilterFlightsCalloutTableView.frame = CGRect(x: 0, y: 121, width: 100, height: 22 * filterFirstLevelOptions.count)
+            sortFilterFlightsCalloutTableView.reloadData()
+            self.sortFilterFlightsCalloutView.contentView = sortFilterFlightsCalloutTableView
+            self.sortFilterFlightsCalloutView.isHidden = false
+            self.sortFilterFlightsCalloutView.animation(withType: .stretch, presenting: true)
+            self.sortFilterFlightsCalloutView.permittedArrowDirection = .up
+            var calloutRect: CGRect = CGRect.zero
+            calloutRect.origin = CGPoint(x: filterButton.frame.midX, y: CGFloat(59))
+            self.sortFilterFlightsCalloutView.presentCallout(from: calloutRect, in: self.view, constrainedTo: self.view, animated: true)
+
         } else {
             self.sortFilterFlightsCalloutView.dismissCallout(animated: true)
             self.sortFilterFlightsCalloutView.isHidden = true
         }
     }
     @IBAction func sortFlightsButtonTouchedUpInside(_ sender: Any) {
+        if self.sortFilterFlightsCalloutView.isHidden == true || (self.sortFilterFlightsCalloutView.isHidden == false && calloutTableViewMode == "filter"){
+            calloutTableViewMode = "sort"
+            sortFilterFlightsCalloutTableView.frame = CGRect(x: 0, y: 121, width: 140, height: 22 * sortFirstLevelOptions.count)
+            sortFilterFlightsCalloutTableView.reloadData()
+            self.sortFilterFlightsCalloutView.contentView = sortFilterFlightsCalloutTableView
+            self.sortFilterFlightsCalloutView.isHidden = false
+            self.sortFilterFlightsCalloutView.animation(withType: .stretch, presenting: true)
+            self.sortFilterFlightsCalloutView.permittedArrowDirection = .up
+            var calloutRect: CGRect = CGRect.zero
+            calloutRect.origin = CGPoint(x: sortButton.frame.midX, y: CGFloat(59))
+            self.sortFilterFlightsCalloutView.presentCallout(from: calloutRect, in: self.view, constrainedTo: self.view, animated: true)
+            
+        } else {
+            self.sortFilterFlightsCalloutView.dismissCallout(animated: true)
+            self.sortFilterFlightsCalloutView.isHidden = true
+        }
     }
-    
-    
 }
